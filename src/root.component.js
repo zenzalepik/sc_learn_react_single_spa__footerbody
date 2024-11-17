ff29283ee8aefe4feb127781b460db604324677f
Zen Zalepik <51511976+zenzalepik@users.noreply.github.com>
2024-11-17 17:02:02 +0700

    push

---
+++ src/root.component.js
@@ -0,0 +1,17 @@
+export default function Root(props) {
+  return (
+    <footer
+      style={{
+        left: 0,
+        bottom: 0,
+        width: "100%",
+        backgroundColor: "#333",
+        color: "white",
+        textAlign: "center",
+        padding: 10
+      }}
+    >
+      <p>©2022, Belajar Microfrotend.</p>
+    </footer>
+  );
+}
